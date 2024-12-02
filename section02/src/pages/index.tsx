b770338ba61baf818d46afc41f37d3c284f1bb63
Sam <wtawonlife@gmail.com>
2024-12-02 17:38:19 +0900

    SSR Practice - fetching datas in home page

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -3,19 +3,40 @@ import styles from "./index.module.css";
 import {ReactNode} from "react";
 import book from "@/mock/books.json";
 import BookItem from "@/components/book-item";
+import fetchBooks from "@/lib/fetch-books";
+import { InferGetServerSidePropsType } from "next";
+import fetchRandomBooks from "@/lib/fetch-random-books";
 
-export default function Home() {
+export const getServerSideProps = async() => {
+    // const allBooks = await fetchBooks();
+    // const recoBooks = await fetchRandomBooks();
+
+    const [allBooks, recoBooks] = await Promise.all([
+        fetchBooks(),
+        fetchRandomBooks(),
+    ])
+
+     return {
+         props: {
+             allBooks,
+             recoBooks,
+         }
+     }
+}
+
+export default function Home({allBooks, recoBooks}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+    
     return <div className={styles.container}>
         <section>
             <h3>지금 추천하는 도서</h3>
-            {book.map((book) => <BookItem key={book.id} {...book}/>)}
+            {recoBooks.map((book) => <BookItem key={book.id} {...book}/>)}
         </section>
         <section>
             <h3>등록된 모든 도서</h3>
-            {book.map((book) => <BookItem key={book.id} {...book}/>)}
+            {allBooks.map((book) => <BookItem key={book.id} {...book}/>)}
         </section>
     </div>
-}
+};
 
 Home.getLayout = (page: ReactNode) => {
     return <SearchableLayout>{page}</SearchableLayout>
