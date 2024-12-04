6714aff1f3da3da6e6c877c9a9c3684fd2543968
Sam <wtawonlife@gmail.com>
2024-12-04 18:26:01 +0900

    SSG Practice - 동적 route page에 getStaticPaths 적용

--- section02/src/pages/book/[id].tsx
+++ section02/src/pages/book/[id].tsx
@@ -1,4 +1,4 @@
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import { GetServerSidePropsContext, GetStaticPropsContext, InferGetServerSidePropsType, InferGetStaticPropsType } from "next";
 import styles from "./[id].module.css";
 import fetchOneBook from "@/lib/fetch-one-book";
 
@@ -13,7 +13,19 @@ const mockData = {
     "coverImgUrl": "https://shopping-phinf.pstatic.net/main_3247334/32473346832.20221227204218.jpg"
 };
 
-export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+export const getStaticPaths = () => {
+    return {
+        paths: [
+            {params: { id: "1"}},
+            {params: {id: "2"}},
+            {params: {id: "3"}}
+        ],
+        fallback: "blocking"
+    }
+}
+
+
+export const getStaticProps = async (context: GetStaticPropsContext) => {
 
     const id = context.params!.id;
     const book = await fetchOneBook(Number(id));
@@ -26,7 +38,7 @@ export const getServerSideProps = async (context: GetServerSidePropsContext) => 
 }
 
 
-function Page({book}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+function Page({book}: InferGetStaticPropsType<typeof getStaticProps>) {
 
     if(!book) return "문제가 발생했습니다. 다시 시도해 주세요."
     const {id, title, subTitle, description, author, publisher, coverImgUrl} = book;
