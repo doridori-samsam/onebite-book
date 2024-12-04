e9a592999ac296734a0c7f500f6da37d5f83fc5f
Sam <wtawonlife@gmail.com>
2024-12-04 15:38:46 +0900

    SSG Practice - 사전 렌더링에서 빌드 타임에 query 가져오지 못하는 경우 코드 적용

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,22 +1,41 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import SearchableLayout from "@/components/searchable-layout";
 import BookItem from "@/components/book-item";
 import fetchBooks from "@/lib/fetch-books";
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import { GetServerSidePropsContext, GetStaticPropsContext, InferGetServerSidePropsType } from "next";
+import { useRouter } from "next/router";
+import { BookData } from "@/types";
 
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext
-) => {
-  const q = context.query.q;
-  const books = await fetchBooks(q as string);
+// export const getStaticProps = async (
+//   context: GetStaticPropsContext
+// ) => {
+//   const q = context.query.q; //사전 정적 페이지에서 query를 가져올 수 없음...
+//   const books = await fetchBooks(q as string);
+//
+//   return {
+//     props: {
+//       books,
+//     },
+//   };
+// };
 
-  return {
-    props: {
-      books,
-    },
-  };
-};
-function Page({ books }:InferGetServerSidePropsType<typeof getServerSideProps>) {
+
+function Page() {
+  const router = useRouter()
+  const q = router.query.q;
+
+  const [books, setBooks] = useState<BookData[]>([]);
+
+  const fetchSearchResult = async () => {
+    const data = await fetchBooks(q as string);
+    setBooks(data);
+  }
+
+  useEffect(() => {
+    if(q)[
+        fetchSearchResult()
+    ]
+  }, [q]);
   
   return (
     <div>
